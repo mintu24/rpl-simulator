01ba5e215134b02a245a3f7124dca8b73659a2ef
ali.moshfegh <ali.moshfegh@18a18238-fbe1-11de-9905-3f11d370f582>
2010-01-18 20:08:37 +0000

    compatibility with gtk 2.16, makefile/gcc round fix

--- src/gui/mainwin.h
+++ src/gui/mainwin.h
@@ -11,6 +11,10 @@
 #define MAIN_WIN_WIDTH		800
 #define MAIN_WIN_HEIGHT		600
 
+#ifndef gtk_widget_set_visible
+#define gtk_widget_set_visible(widget, visible) {if (visible) gtk_widget_show(widget); else gtk_widget_hide(widget);}
+#endif
+
 
 void                main_win_init();
 
