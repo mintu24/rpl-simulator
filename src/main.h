6ded9e160c2f6c0d047816acdca72346d28a2660
ccrisan <ccrisan@18a18238-fbe1-11de-9905-3f11d370f582>
2010-01-21 21:37:04 +0000

    added *all functionalities

--- src/main.h
+++ src/main.h
@@ -10,11 +10,22 @@
 
 extern                  GThread *rs_main_thread;
 
-node_t *                rs_add_node();
-void                    rs_rem_node(node_t *node);
-void                    rs_rem_all_nodes();
-void                    rs_load_params(char *filename);
+
+void                    rs_open();
+void                    rs_save();
 void                    rs_quit();
 
+void                    rs_start();
+void                    rs_stop();
+
+node_t *                rs_add_node();
+void                    rs_rem_node(node_t *node);
+void                    rs_wake_node(node_t *node);
+void                    rs_kill_node(node_t *node);
+
+void                    rs_add_more_nodes();
+void                    rs_rem_all_nodes();
+void                    rs_wake_all_nodes();
+void                    rs_kill_all_nodes();
 
 #endif /* MAIN_H_ */
