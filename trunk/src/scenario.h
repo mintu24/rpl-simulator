209faa9719d277c34e21a68d3078f7c51d1629be
ccrisan <ccrisan@18a18238-fbe1-11de-9905-3f11d370f582>
2010-02-28 00:51:57 +0000

    initial support for load/save scenarios

---
+++ trunk/src/scenario.h
@@ -0,0 +1,12 @@
+
+#ifndef SCENARIO_H_
+#define SCENARIO_H_
+
+#include "base.h"
+
+
+char *                      scenario_load(char *filename);
+char *                      scenario_save(char *filename);
+
+
+#endif /* SCENARIO_H_ */
